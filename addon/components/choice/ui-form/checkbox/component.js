6b28d577a7413a5b418e530b9690304c16d69b80
余凡 <albert@cform.io>
2016-09-06 16:23:33 +0800

    Add choice/ui-form w/ checkbox and radio components

---
+++ addon/components/choice/ui-form/checkbox/component.js
@@ -0,0 +1,6 @@
+import Component from 'ember-form-for/components/form-fields/checkbox-field';
+import layout from './template';
+
+export default Component.extend({
+  layout,
+});
---
+++ addon/components/choice/ui-form/radio/component.js
@@ -0,0 +1,6 @@
+import Component from 'ember-form-for/components/form-fields/radio-field';
+import layout from './template';
+
+export default Component.extend({
+  layout,
+});
