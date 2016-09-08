0a63f1e529f8e20f19d01089e2a0934656d97c6a
余凡 <albert@cform.io>
2016-09-08 15:10:50 +0800

    Refactor dummy app

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -7,11 +7,6 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('ui-button');
-
-  this.route('ui-form');
-
-  this.route('ui-panel');
 });
 
 export default Router;
---
+++ tests/dummy/app/routes/index.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+import Route from 'ember-route';
+import { isNone } from 'ember-utils';
+
+const model = Ember.Object.create({
+  form: Ember.Object.create({
+    checkall: true, 
+    inversed: false, 
+    gender: 1, 
+    title: '巧思问卷标题'
+  })
+});
+
+export default Route.extend({
+  queryParams: {
+    component: {
+      refreshModel: true
+    }
+  },
+
+  model(params) {
+    return {
+      components: [
+        'ui-button', 'ui-form', 'ui-panel'
+      ],
+      component: params.component
+    };
+  },
+
+  renderTemplate(controller, { component }) {
+    if (isNone(component)) {
+      return this._super(...arguments);
+    }
+
+    this.render(`choice.${component}`, { model });
+  }
+});
