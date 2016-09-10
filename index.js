dd054b61b7b122d535870a8c6f5c834a2fa948d8
余凡 <albert@cform.io>
2016-09-10 17:46:49 +0800

    Use virtual modules to expose color variables

--- index.js
+++ index.js
@@ -2,35 +2,50 @@
 
 module.exports = {
   name: 'ember-choice-ui',
+
   isDevelopingAddon() { return true },
 
-  options: {
-    cssModules: {
-      plugins: {
-        before: [
+  included: function(app, addon) {
+    this.options = Object.assign({}, this.options, {
+      cssModules: {
+        plugins: [
           require('postcss-sassy-mixins'),
           require('postcss-import'),
           require('lost'),
           require('postcss-cssnext')({warnForDuplicates: false}),
-          require('rucksack-css')({fallbacks: true})
-        ],
-        after: [
+          require('rucksack-css')({fallbacks: true}),
           require('cssnano')({safe: true}),
           require('postcss-browser-reporter')
-        ]
+        ],
+        virtualModules: {
+          'choice-ui/colors': {
+            'color-blue': '#1894f2',
+            'color-gray': '#919ba2',
+            'color-green': '#2ecc71',
+            'color-slate': '#4e5b68',
+            'color-golden': '#f1c40f',
+            'color-purple': '#5940aa',
+            'color-silver': '#bdc3c7',
+            'color-tomato': '#e74c3c'
+          }
+        }
+      },
+      outputPaths: {
+        app: {
+          css: {'app': '/assets/bundle.css'},
+          js: '/assets/bundle.js',
+        },
       }
-    }
-  },
+    });
 
-  included: function(app, addon) {
-    const target = addon || app;
+    this._super.included.apply(this, arguments);
 
-    this._super.included.call(this, target);
+    this.app = addon || app;
 
-    target.import(
+    this.app.import(
       `./bower_components/normalize-css/normalize.css`, { prepend: true }
     );
 
-    target.import('./vendor/register-version.js');
+    this.app.import('./vendor/register-version.js');
   }
 };
